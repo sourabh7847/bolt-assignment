eecf476109791f9385647c11cfc770ceccffdf26
sourabh7847 <flank.valish@gmail.com>
2025-04-14 01:29:44 +0530

    ...

--- remix.config.js
+++ remix.config.js
@@ -1,18 +1,18 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-    future: {
-      // Enable future Remix features
-      v3_fetcherPersist: true,
-      v3_relativeSplatPath: true,
-      v3_throwAbortReason: true,
-    },
-    ignoredRouteFiles: ['**/.*'], // ignore dotfiles
-    serverModuleFormat: 'esm',
-    serverPlatform: 'neutral',
-    appDirectory: 'app',
-    assetsBuildDirectory: 'public/build',
-    publicPath: '/build/',
-    serverBuildDirectory: 'build/server',
-    server: false, // let Cloudflare handle the server
-  };
-  
+  future: {
+    // Enable future Remix features
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+    v3_throwAbortReason: true,
+  },
+  ignoredRouteFiles: ['**/.*'], // ignore dotfiles
+  serverModuleFormat: 'esm',
+  serverPlatform: 'neutral',
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'build/server',
+  serverBuildTarget: 'netlify',
+  server: './server.js',
+};
